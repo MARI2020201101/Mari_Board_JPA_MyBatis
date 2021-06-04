77bb39a306d7aee1746e5f727865f0d34d642f65
MARI <mari20201101@gmail.com>
2021-06-04 14:38:42 +0900

    Add applicationContext test file

---
+++ src/test/java/com/mariworld/bootboardjpamybatis/ApplicationContextTest.java
@@ -0,0 +1,22 @@
+package com.mariworld.bootboardjpamybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class ApplicationContextTest {
+    @Autowired
+    ApplicationContext ac;
+
+    @Test
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + "" +
+                    ", object = " + bean);
+        }
+    }
+}
--- src/test/java/com/mariworld/bootboardjpamybatis/mapper/ConnectionTest.java
+++ src/test/java/com/mariworld/bootboardjpamybatis/mapper/ConnectionTest.java
@@ -1,8 +1,12 @@
 package com.mariworld.bootboardjpamybatis.mapper;
 
+import com.mariworld.bootboardjpamybatis.BootBoardJpaMybatisApplication;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
